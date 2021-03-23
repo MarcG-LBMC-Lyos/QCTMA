1c602bf901dc8dd8c552b0f8ab3ca7fcfb568701
MarcG <m.gardegaront@gmail.com>
2021-03-23 15:26:59 +0100

    Better description of init method of qctma class.

--- qctma1.0.x/setup.py
+++ qctma1.0.x/setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 
 setup(name='qctma',
-      version='1.0.1',
+      version='1.0.2',
       description="Injects material (Young's modulus) to each element, based on a Dicom stack, and gray level to Young's"
                   "modulus relationships. Specifically designed to be used with Ansys .cdb meshes.",
+      long_description="Injects material (Young's modulus) to each element, based on a Dicom stack, and gray level to Young's"
+                       "modulus relationships. Specifically designed to be used with Ansys .cdb meshes.",
       url='https://github.com/MarcG-LBMC-Lyos/QCTMA',
       author='Marc Gardegaront',
       author_email='m.gardegaront@gmail.com',
