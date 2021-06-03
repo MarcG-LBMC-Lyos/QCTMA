3f39583cd15717f2a1e63b530dfd231e61f1c570
MarcG <m.gardegaront@gmail.com>
2021-06-03 22:52:38 +0200

    - Removed unnecessary packages from setuptool and requirements.

--- qctma1.0.x/setup.py
+++ qctma1.0.x/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='qctma',
-      version='1.0.11',
+      version='1.0.12',
       description="Injects material (Young's modulus) to each element, based on a Dicom stack, and gray level to Young's"
                   "modulus relationships. Specifically designed to be used with Ansys .cdb meshes.",
       long_description="Injects material (Young's modulus) to each element, based on a Dicom stack, and gray level to Young's"
@@ -12,7 +12,7 @@ setup(name='qctma',
       author_email='m.gardegaront@gmail.com',
       license='GNU GPLv3',
       py_modules=['qctma', 'rw_cdb'],
-      install_requires=['matplotlib>=3.3.4', 'numpy>=1.20.1', 'pydicom>=2.1.2', 'quadpy>=0.16.6', 'scipy>=1.6.1',
+      install_requires=['matplotlib>=2.2.5', 'numpy>=1.19.5', 'pydicom>=2.1.2', 'quadpy>=0.16.5', 'scipy>=1.5.4',
                         'reportlab>=3.5.66'
                         ],
       python_requires=">=3.6")
