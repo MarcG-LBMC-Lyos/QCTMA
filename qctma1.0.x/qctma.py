0d75c211bb1b4ad5ad8ebc4daea546cd6b255a71
gardegaront <m.gardegaront@gmail.com>
2024-11-04 17:00:49 +0100

    Changed version

--- qctma1.0.x/qctma.py
+++ qctma1.0.x/qctma.py
@@ -19,7 +19,7 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
 
 
-__version__ = "1.0.25"
+__version__ = "1.0.26"
 
 class qctma(object):
     """
